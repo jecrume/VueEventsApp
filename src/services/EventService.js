039820bb97e17a1b9b9427fee95f67d7f758c6b2
jecrume <jecrume@gmail.com>
2021-04-25 19:45:50 -0400

    Created EventService helper to get events from DB

---
+++ src/services/EventService.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+    baseURL: 'http://my-json-server.typicode.com/jecrume/EventsProjectDB',
+    withCredentials:false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default{
+
+    getEvents(){
+        return apiClient.get('/events')
+    }
+}
