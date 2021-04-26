20100da27c1ead01133c63e9e1b34c5d48f16b5a
jecrume <jecrume@gmail.com>
2021-04-25 21:13:58 -0400

    Updated baseURL for https

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/jecrume/EventsProjectDB',
+    baseURL: 'https://my-json-server.typicode.com/jecrume/EventsProjectDB',
     withCredentials:false,
     headers: {
         Accept: 'application/json',
